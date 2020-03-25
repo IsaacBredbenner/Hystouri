22567640245e1e38370b698b25b28a34f1977eaf
Tom Swope <tjswope@gmail.com>
2020-03-25 11:50:46 -0400

    I think i fixed the sigabrt error with logging in. I updated some cocoa pods as well, which will hopefully prevent potential problems in the future.

--- Hystouri/SignUpViewController.swift
+++ Hystouri/SignUpViewController.swift
@@ -14,22 +14,16 @@ class SignUpViewController: UIViewController {
 
     @IBOutlet weak var password: UITextField!
     @IBOutlet weak var emailAddress: UITextField!
-    @IBOutlet weak var userName: UITextField!
     @IBOutlet weak var signUpOutlet: UIButton!
+    
     @IBAction func signUpAction(_ sender: UIButton) {
-        guard let userName = userName.text else {return}
         guard let email = emailAddress.text else {return}
         guard let password = password.text else {return}
 
         Auth.auth().createUser(withEmail: email, password: password){ user, error in
             if let _ = user {
                 print("user created")
-                let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
-                changeRequest?.displayName = userName
-                changeRequest?.commitChanges(completion: { (error) in
-                    print("couldn't change name")
-                    })
-                }
+            }
                 else{
                     print(error.debugDescription)
                 }
@@ -38,10 +32,7 @@ class SignUpViewController: UIViewController {
     
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool{
-        if userName.isFirstResponder {
-            emailAddress.becomeFirstResponder()
-        }
-        else if emailAddress.isFirstResponder{
+        if emailAddress.isFirstResponder{
             password.becomeFirstResponder()
         }
         else{
@@ -53,7 +44,7 @@ class SignUpViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        userName.becomeFirstResponder()
+        emailAddress.becomeFirstResponder()
         // Do any additional setup after loading the view.
     }
     
